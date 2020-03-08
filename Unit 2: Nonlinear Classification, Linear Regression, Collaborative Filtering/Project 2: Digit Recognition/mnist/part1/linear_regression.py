a7284f700fcdb01a097b2b0af2c08e6d30fc9941
Chris Enns <chrisenns137@gmail.com>
2020-03-08 16:29:21 -0700

    Project 2 started.
    Also contains files for Project 3 in Unit 3.

---
+++ Unit 2: Nonlinear Classification, Linear Regression, Collaborative Filtering/Project 2: Digit Recognition/mnist/part1/linear_regression.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+### Functions for you to fill in ###
+
+def closed_form(X, Y, lambda_factor):
+    """
+    Computes the closed form solution of linear regression with L2 regularization
+
+    Args:
+        X - (n, d + 1) NumPy array (n datapoints each with d features plus the bias feature in the first dimension)
+        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
+            data point
+        lambda_factor - the regularization constant (scalar)
+    Returns:
+        theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
+        represents the y-axis intercept of the model and therefore X[0] = 1
+    """
+    # YOUR CODE HERE
+    raise NotImplementedError
+
+### Functions which are already complete, for you to use ###
+
+def compute_test_error_linear(test_x, Y, theta):
+    test_y_predict = np.round(np.dot(test_x, theta))
+    test_y_predict[test_y_predict < 0] = 0
+    test_y_predict[test_y_predict > 9] = 9
+    return 1 - np.mean(test_y_predict == Y)
